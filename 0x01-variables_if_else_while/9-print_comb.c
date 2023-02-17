f8198b7b9a5d9535b303b66329d46af9a28334fc
Halawany1 <mddhdhd44@gmail.com>
2023-02-17 14:54:28 -0800

    a

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+
+for (i = 0 ; i <= 9 ; i++)
+{
+if (i != 9)
+{
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+}
+else
+{
+putchar(i + '0');
+}
+}
+
+
+return (0);
+
+}
