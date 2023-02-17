f6a47870892ee4b1f217cfc935598436ed550252
Halawany1 <mddhdhd44@gmail.com>
2023-02-17 14:36:32 -0800

    a

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char i;
+
+for (i = '0' ; i <= '9' ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
