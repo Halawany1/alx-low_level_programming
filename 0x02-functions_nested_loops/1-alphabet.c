a108f5fb6b31c52a46605426d35393a191d614d6
Halawany1 <mddhdhd44@gmail.com>
2023-02-21 17:27:08 -0800

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 int i;
 for (i = 'a' ; i <= 'z' ; i++)
