b400ea8a5852293ca09ea4e9497d0017167732b6
Halawany1 <mddhdhd44@gmail.com>
2023-02-21 17:15:21 -0800

    a

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - to print the alphabet, in lowercase
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 'a' ; i <= 'z' ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
