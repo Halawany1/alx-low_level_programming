2cc994c5293a715b791134892918977a72937532
Halawany1 <mddhdhd44@gmail.com>
2023-02-27 16:19:59 -0800

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_rev -function that prints a string, in reverse
+ * @s: pointer to char
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s) - 1 ; i >= 0 ; i--)
+		printf("%c", s[i]);
+	printf("\n");
+}
