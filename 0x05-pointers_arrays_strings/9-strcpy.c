a39b96db289f51e42ee127a8d594556ed109b341
Halawany1 <mddhdhd44@gmail.com>
2023-02-27 17:57:31 -0800

    a

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy -function that prints half of string, in reverse
+ * @dest: pointer to char
+ * @src: pointer to char
+ * Return: void
+ */
+char *_strcpy(char *dest, const char *src)
+{
+int i = 0;
+
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
