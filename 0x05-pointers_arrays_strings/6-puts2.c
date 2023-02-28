ad3c4dbfc1a4e849e2559e0c6ec4b18b863d7222
Halawany1 <mddhdhd44@gmail.com>
2023-02-27 17:20:02 -0800

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 -function that prints a string, in reverse
+ * @str: pointer to char
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+int x = strlen(str);
+
+for (i = 0 ; i < x ; i++)
+{
+if (i % 2 == 0)
+printf("%c", str[i]);
+}
+}
