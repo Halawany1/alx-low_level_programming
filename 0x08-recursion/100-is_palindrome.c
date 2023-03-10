77a31752e27737518b04e7f453b837c6a8db70b6
Halawany1 <mddhdhd44@gmail.com>
2023-03-09 16:54:39 -0800

    a

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * isplind- return if string isplindrome or no
+ * @s:string
+ * @a:number
+ * @b:number
+ * Return: number
+*/
+int isplind(char *s, int a, int b)
+{
+if (a >= b)
+{
+return (1);
+}
+if (s[a] == s[b])
+{
+a++;
+b--;
+isplind(s, a, b);
+}
+else
+{
+return (0);
+}
+}
+/**
+ * is_palindrome- return if string isplindrome or no
+ * @s:string
+ * Return: number
+*/
+int is_palindrome(char *s)
+{
+int q = strlen(s) - 1;
+
+return (isplind(s, 0, q));
+}
