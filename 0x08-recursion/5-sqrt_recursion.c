b52d1dbe13cbc017f9e3a85b503003b7495cae30
Halawany1 <mddhdhd44@gmail.com>
2023-03-09 16:12:15 -0800

    a

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "mian.h"
+/**
+ * sqr - given sqrt of numbers
+ * @a:number
+ * @b:number
+ * Return: number
+*/
+int sqr(int a, int b)
+{
+if (b * b == a)
+{
+return (b);
+}
+else if (b * b > a)
+{
+return (-1);
+}
+else
+{
+return (sqr(a, b + 1));
+}
+}
+/**
+ * _sqrt_recursion- function that returns the natural square root of a number
+ * @n:number
+ * Return: number
+*/
+int _sqrt_recursion(int n)
+{
+return (sqr(n, 1));
+}
